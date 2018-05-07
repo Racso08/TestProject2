2350fae832519c7c14d732e0ea4bebfb005b56f5
Racso08 <oscar080899@gmail.com>
2018-05-06 19:31:43 -0600

    Cambio drastico

--- main.cpp
+++ main.cpp
@@ -1,42 +1,50 @@
-/* fread example: read an entire file */
 #include <iostream>
-#include <stdio.h>
-#include <stdlib.h>
-#include <string>         // std::string
+#include <fstream>
 
 using namespace std;
 
-int main () {
-    FILE * pFile;
-    long lSize;
-    char * buffer;
-    size_t result;
+void ReadFile(char *name)
+{
+    FILE *file;
+    char *buffer;
+    unsigned long fileLen;
 
-    pFile = fopen ( "/home/racso/Project#2/Bella.mp3" , "rb" );
-    if (pFile==NULL) {fputs ("File error",stderr); exit (1);}
-
-    // obtain file size:
-    fseek (pFile , 0 , SEEK_END);
-    lSize = ftell (pFile);
-    rewind (pFile);
-
-    // allocate memory to contain the whole file:
-    buffer = (char*) malloc (sizeof(char)*lSize);
-    if (buffer == NULL) {fputs ("Memory error",stderr); exit (2);}
-
-    // copy the file into the buffer:
-    //result = fread (buffer,1,lSize,pFile);
-    //if (result != lSize) {fputs ("Reading error",stderr); exit (3);}
-
-    result = fread (buffer,1,200,pFile);//read 200 bytes to test only
-
-    //For loop added by me
-    for( unsigned i = 0; i <= result; ++i ) {
-        printf("%x",buffer[i]);
+    //Open file
+    file = fopen(name, "rb");
+    if (!file)
+    {
+        fprintf(stderr, "Unable to open file %s", name);
+        return;
     }
-    /* the whole file is now loaded in the memory buffer. */
-    // terminate
-    fclose (pFile);
-    free (buffer);
+
+    //Get file length
+    fseek(file, 0, SEEK_END);
+    fileLen=ftell(file);
+    fseek(file, 0, SEEK_SET);
+
+    //Allocate memory
+    buffer=(char *)malloc(fileLen+1);
+    if (!buffer)
+    {
+        fprintf(stderr, "Memory error!");
+        fclose(file);
+        return;
+    }
+
+    //Read file contents into buffer
+    fread(buffer, fileLen, 1, file);
+    fclose(file);
+
+    //Do what ever with buffer
+    for(int i = 0; i<200; i++){
+        printf("%X",buffer[i]);
+    }
+
+    free(buffer);
+}
+
+int main(){
+    cout<<"TEST"<<endl;
+    ReadFile("/home/racso/Project#2/Bella.mp3");
     return 0;
 }
